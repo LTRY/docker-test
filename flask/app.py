fe85b89d2f3aa976a096dfb91a98777455a7e3c4
LTY <50324941+LTRY@users.noreply.github.com>
2020-05-18 18:15:39 +0200

    Add files via upload

--- flask/app.py
+++ flask/app.py
@@ -1,33 +1,31 @@
 from flask import Flask, request, jsonify, Response
 import json
 import mysql.connector
-from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 
 def getMysqlConnection():
     return mysql.connector.connect(user='root', host='db', port='3306', password='root', database='dockertest')
 
-@app.route("/")
+@app.route('/')
 def hello():
-    return "Flask inside Docker!!"
+    return 'Docker-Flask-test'
 
 @app.route('/api/test', methods=['GET'])
-@cross_origin() # allow all origins all methods.
-def get_months():
+def sql_test():
     db = getMysqlConnection()
     cur = db.cursor()
     try:
-        cur.execute('CREATE TABLE tabletest (name VARCHAR(20));')
-        cur.execute('INSERT INTO tabletest (name) VALUES (%(name)s);', {'name':'hello from sql'})
+        cur.execute('DROP TABLE tabletest;')
     except:
         pass
-    
+    cur.execute('CREATE TABLE tabletest (name VARCHAR(20));')
+    cur.execute('INSERT INTO tabletest (name) VALUES (%(name)s);', {'name':'hello from sql'})
     cur.execute('SELECT * from tabletest;')
     rep = [name for name in cur][0]
     db.close()
 
     return rep
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
